dc5ae7af2cdcbb0ce4b2f5ca37016c2a533d0b65
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 13:30:35 +0700

    Refactor extracted component

--- app/components/MainContent.tsx
+++ app/components/MainContent.tsx
@@ -1,9 +1,6 @@
 import React from "react";
+import MainMenu from "./MainMenu";
 
 export default function MainContent() {
-  return (
-    <div className="bg-blue-500 flex-grow flex items-center justify-center">
-      <span className="text-white">MAIN</span>
-    </div>
-  );
+  return <MainMenu />;
 }
