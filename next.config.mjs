79ba6c40969c5c17a685f9c3e5db197350445bd2
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 12:55:57 +0700

    Add unsplash

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com"], // Add external domains here
+  },
+};
 
 export default nextConfig;
