940d3b3ee1c8e3907754f5401979973ef9da0529
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 12:04:14 +0700

    Hero comp

---
+++ app/components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="bg-green-500 h-24 flex items-center justify-center">
+      <span className="text-white">HERO</span>
+    </div>
+  );
+}
