401dca7a4e57ea1d1793fd85b07a3e4391b6de84
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 12:04:23 +0700

    Main content comp

---
+++ app/components/MainContent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MainContent() {
+  return (
+    <div className="bg-blue-500 flex-grow flex items-center justify-center">
+      <span className="text-white">MAIN</span>
+    </div>
+  );
+}
