f88173dd09647125358957a43da641cc222c2675
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 13:02:42 +0700

    Add 5 total hero data

--- app/data/heroData.ts
+++ app/data/heroData.ts
@@ -17,4 +17,16 @@ export const heroData = [
     title: "Third Slide",
     desc: "This is the description for the third slide.",
   },
+  {
+    image:
+      "https://images.unsplash.com/photo-1483366774565-c783b9f70e2c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Forth Slide",
+    desc: "This is the description for the forth slide.",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1460574283810-2aab119d8511?q=80&w=2063&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Fifth Slide",
+    desc: "This is the description for the fifth slide.",
+  },
 ];
