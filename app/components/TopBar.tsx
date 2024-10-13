cf22f143f0f12b9a0b923d82b52575eba1eb9a88
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 12:04:31 +0700

    Top bar comp

---
+++ app/components/TopBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TopBar() {
+  return (
+    <div className="bg-red-500 h-16 flex items-center justify-center sticky top-0 z-10">
+      <span className="text-white">TOP BAR</span>
+    </div>
+  );
+}
