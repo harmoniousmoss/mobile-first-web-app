ca3e9c7b953521e86428c72843c87befa392392e
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 12:12:29 +0700

    Add search field and avatar icon

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { FiSearch, FiUser } from "react-icons/fi";
 
 export default function TopBar() {
   return (
-    <div className="bg-red-500 h-16 flex items-center justify-center sticky top-0 z-10">
-      <span className="text-white">TOP BAR</span>
+    <div className="bg-red-500 h-16 flex items-center justify-between sticky top-0 z-10 px-4">
+      {/* Search Field */}
+      <div className="flex items-center bg-white rounded-full px-3 py-2 flex-grow max-w-xs">
+        <FiSearch className="text-gray-400 mr-2" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="outline-none text-gray-700 flex-grow"
+        />
+      </div>
+
+      {/* User Icon */}
+      <div className="ml-4 bg-white rounded-full h-10 w-10 flex items-center justify-center">
+        <FiUser className="text-gray-500" size={20} />
+      </div>
     </div>
   );
 }
