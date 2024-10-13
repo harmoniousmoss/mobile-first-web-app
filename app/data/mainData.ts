82ebdad40d8930057f081125af4950d62da8e0e0
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 13:31:02 +0700

    Main menu const data

---
+++ app/data/mainData.ts
@@ -0,0 +1,19 @@
+import {
+  FiTarget,
+  FiPhoneCall,
+  FiBriefcase,
+  FiGlobe,
+  FiFileText,
+  FiUsers,
+  FiMic,
+} from "react-icons/fi";
+
+export const main = [
+  { name: "About", icon: FiGlobe },
+  { name: "Focus Area", icon: FiTarget },
+  { name: "News", icon: FiMic },
+  { name: "Talents", icon: FiBriefcase },
+  { name: "Vacancy", icon: FiUsers },
+  { name: "Tender", icon: FiFileText },
+  { name: "Contact", icon: FiPhoneCall },
+];
