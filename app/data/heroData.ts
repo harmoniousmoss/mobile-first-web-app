b6e32b87528e4daf50890b40ec5950623250736e
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 12:56:15 +0700

    Hero data init

---
+++ app/data/heroData.ts
@@ -0,0 +1,20 @@
+export const heroData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "First Slide",
+    desc: "This is the description for the first slide.",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1449824913935-59a10b8d2000?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Second Slide",
+    desc: "This is the description for the second slide.",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1427694012323-fb5e8b0c165b?q=80&w=2089&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Third Slide",
+    desc: "This is the description for the third slide.",
+  },
+];
