24ff611d3b617f6c348c19d595626e69dcf7b368
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 13:30:52 +0700

    Buttom Bar const data

---
+++ app/data/bottomBarData.ts
@@ -0,0 +1,8 @@
+import { FiMic, FiBriefcase, FiPhoneCall, FiHome } from "react-icons/fi";
+
+export const bottomBarItems = [
+  { name: "Home", icon: FiHome },
+  { name: "News", icon: FiMic },
+  { name: "Opportunity", icon: FiBriefcase },
+  { name: "Contact", icon: FiPhoneCall },
+];
