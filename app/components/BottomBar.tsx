a728bae8592bb54ed27e9d25cb46d687e111a5b3
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-13 12:04:07 +0700

    bottom bar comp

---
+++ app/components/BottomBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function BottomBar() {
+  return (
+    <div className="bg-red-500 h-16 flex items-center justify-center sticky bottom-0 z-10">
+      <span className="text-white">BOTTOM BAR</span>
+    </div>
+  );
+}
